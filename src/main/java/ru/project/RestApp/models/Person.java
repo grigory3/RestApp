8c0d31541300cacb24f92387e0e79e12d5e81c4c
grigory <grigory@restApp.com>
2024-04-01 15:23:05 +0300

    added dependency to check values from users and annotations in person class

--- src/main/java/ru/project/RestApp/models/Person.java
+++ src/main/java/ru/project/RestApp/models/Person.java
@@ -1,6 +1,10 @@
 package ru.project.RestApp.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "Person")
@@ -11,10 +15,18 @@ public class Person {
     @GeneratedValue(strategy =GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "name")
+    @NotEmpty(message = "name should be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
+    @Column(name = "age")
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
 
+    @Column(name = "email")
+    @Email
+    @NotEmpty(message = "Email should not be empty")
     private String email;
 
     public Person() {
